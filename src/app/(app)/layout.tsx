2958f072516a2530128d01be78a6a5bbfb9ec79a
peack <poliouxx@gmail.com>
2024-08-13 22:45:23 +0200

    fixes tw issues

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
+import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from '@/components/Layout/Header'
-import './globals.css'
 
 const inter = Inter({
   subsets: ['latin'],
