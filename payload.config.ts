c781178ed9c1a2fcdd0abe12cc26b2a51deaa840
peack <poliouxx@gmail.com>
2024-08-14 18:46:22 +0200

    try to update config to match example

--- payload.config.ts
+++ payload.config.ts
@@ -34,29 +34,11 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
-  //editor: slateEditor({}),
+  admin: {
+    user: Users.slug,
+  },
+  collections: [Users, Media, Products],
   editor: lexicalEditor(),
-  collections: [
-    Users,
-    Media,
-    {
-      slug: 'pages',
-      admin: {
-        useAsTitle: 'title',
-      },
-      fields: [
-        {
-          name: 'title',
-          type: 'text',
-        },
-        {
-          name: 'content',
-          type: 'richText',
-        },
-      ],
-    },
-    Products,
-  ],
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
@@ -66,58 +48,105 @@ export default buildConfig({
       connectionString: process.env.POSTGRES_URI || '',
     },
   }),
-  // db: mongooseAdapter({
-  //   url: process.env.MONGODB_URI || '',
-  // }),
-
   plugins: [
     seoPlugin({
-      collections: ['pages', 'products'],
+      collections: ['products'],
       uploadsCollection: 'media',
     }),
     vercelBlobStorage({
       collections: {
         [Media.slug]: true,
       },
-      token: process.env.READ_WRITE_TOKEN || '',
+      token: process.env.BLOB_READ_WRITE_TOKEN || '',
     }),
   ],
-  /**
-   * Payload can now accept specific translations from 'payload/i18n/en'
-   * This is completely optional and will default to English if not provided
-   */
-  i18n: {
-    supportedLanguages: { en },
-  },
-
-  admin: {
-    autoLogin: {
-      email: 'dev@payloadcms.com',
-      password: 'test',
-      prefillOnly: true,
-    },
-  },
-  async onInit(payload) {
-    const existingUsers = await payload.find({
-      collection: 'users',
-      limit: 1,
-    })
-
-    if (existingUsers.docs.length === 0) {
-      await payload.create({
-        collection: 'users',
-        data: {
-          email: 'dev@payloadcms.com',
-          password: 'test',
-        },
-      })
-    }
-  },
-  // Sharp is now an optional dependency -
-  // if you want to resize images, crop, set focal point, etc.
-  // make sure to install it and pass it to the config.
-
-  // This is temporary - we may make an adapter pattern
-  // for this before reaching 3.0 stable
-  sharp,
 })
+
+// export default buildConfig({
+//   //editor: slateEditor({}),
+//   editor: lexicalEditor(),
+//   collections: [
+//     Users,
+//     Media,
+//     {
+//       slug: 'pages',
+//       admin: {
+//         useAsTitle: 'title',
+//       },
+//       fields: [
+//         {
+//           name: 'title',
+//           type: 'text',
+//         },
+//         {
+//           name: 'content',
+//           type: 'richText',
+//         },
+//       ],
+//     },
+//     Products,
+//   ],
+//   secret: process.env.PAYLOAD_SECRET || '',
+//   typescript: {
+//     outputFile: path.resolve(dirname, 'payload-types.ts'),
+//   },
+//   db: postgresAdapter({
+//     pool: {
+//       connectionString: process.env.POSTGRES_URI || '',
+//     },
+//   }),
+//   // db: mongooseAdapter({
+//   //   url: process.env.MONGODB_URI || '',
+//   // }),
+
+//   plugins: [
+//     seoPlugin({
+//       collections: ['pages', 'products'],
+//       uploadsCollection: 'media',
+//     }),
+//     vercelBlobStorage({
+//       collections: {
+//         [Media.slug]: true,
+//       },
+//       token: process.env.READ_WRITE_TOKEN || '',
+//     }),
+//   ],
+//   /**
+//    * Payload can now accept specific translations from 'payload/i18n/en'
+//    * This is completely optional and will default to English if not provided
+//    */
+//   i18n: {
+//     supportedLanguages: { en },
+//   },
+
+//   admin: {
+//     autoLogin: {
+//       email: 'dev@payloadcms.com',
+//       password: 'test',
+//       prefillOnly: true,
+//     },
+//   },
+//   async onInit(payload) {
+//     const existingUsers = await payload.find({
+//       collection: 'users',
+//       limit: 1,
+//     })
+
+//     if (existingUsers.docs.length === 0) {
+//       await payload.create({
+//         collection: 'users',
+//         data: {
+//           email: 'dev@payloadcms.com',
+//           password: 'test',
+//         },
+//       })
+//     }
+//   },
+//   // Sharp is now an optional dependency -
+//   // if you want to resize images, crop, set focal point, etc.
+//   // make sure to install it and pass it to the config.
+
+//   // This is temporary - we may make an adapter pattern
+//   // for this before reaching 3.0 stable
+//   sharp,
+// })
