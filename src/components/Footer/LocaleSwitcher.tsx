e9880f20193211fd241b6bcdcda68187d6c7f16e
peack <poliouxx@gmail.com>
2024-08-21 00:38:05 +0200

    update flair on current locale

--- src/components/Footer/LocaleSwitcher.tsx
+++ src/components/Footer/LocaleSwitcher.tsx
@@ -1,19 +1,21 @@
 'use client'
 import { Button } from '@components/ui/button'
-import { useTranslations } from 'next-intl'
+import { useLocale, useTranslations } from 'next-intl'
 
 export default function LocaleSwitcher() {
   const t = useTranslations('LocaleSwitcher')
 
   const changeLocale = (locale: string) => {
     document.cookie = `NEXT_LOCALE=${locale}; path=/; max-age=${60 * 60 * 24 * 7}; secure;`
-
     // setTimeout(() => window.document.location.reload(), 1500)
   }
+  const currentLocale = useLocale()
+
   return (
-    <div className="flex gap-1">
+    <div className="flex">
       <Button
         variant={'link'}
+        className={currentLocale === 'fr' ? 'font-bold' : ''}
         onClick={() => {
           changeLocale('fr')
         }}
@@ -22,6 +24,7 @@ export default function LocaleSwitcher() {
       </Button>
       <Button
         variant={'link'}
+        className={currentLocale === 'en' ? 'font-bold' : ''}
         onClick={() => {
           changeLocale('en')
         }}
