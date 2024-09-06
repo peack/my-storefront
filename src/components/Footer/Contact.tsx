ec41bc98b6768f8d40de10323624917f23f27ad0
peack <poliouxx@gmail.com>
2024-09-06 18:36:33 +0200

    add footer actions

--- src/components/Footer/Contact.tsx
+++ src/components/Footer/Contact.tsx
@@ -1,9 +1,22 @@
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+
 export default function Contact() {
   return (
     <>
       <div className="flex-col items-end ">
-        <div>Phone: +1 (555) 555-5555</div>
-        <div>Email: 0sHqZ@example.com</div>
+        <div>
+          Phone:
+          <Link href="tel:+1(555)555-5555">
+            <Button variant={'link'}>555-555-5555</Button>
+          </Link>
+        </div>
+        <div>
+          Email:
+          <Link href="mailto:0sHqZ@example.com">
+            <Button variant={'link'}>0sHqZ@example.com</Button>
+          </Link>
+        </div>
       </div>
     </>
   )
